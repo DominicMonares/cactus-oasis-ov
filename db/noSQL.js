a47d4ee6fc548e2ac0133849d13486040f87b780
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-01-27 22:40:20 -0800

    mongo schema draft

--- db/noSQL.js
+++ db/noSQL.js
@@ -1,1 +1,46 @@
-// decide between Cassandra or Mongo
+/*
+
+decide between Cassandra or Mongo
+
+Cassandra:
+- stored in non-relational partitions
+- good for highly scalable cloud apps, multiple machines
+- table data structure
+- has its own query language'
+
+Mongo: - WINNER
+- stored in json doc
+- ideal for single view data app
+- limited scalability
+- object data structure
+- uses queries structured into JSON fragments
+
+*/
+
+const mongoose = require('mongoose');
+// mongoose.connect('')
+
+let productSchema = new mongoose.Schema({
+  id: {type: Number, required: true, unique: true},
+  campus: {type: String, default: 'hr-rpp'},
+  name: {type: String, required: true},
+  slogan: {type: String, required: true},
+  description: {type: String, required: true},
+  category: {type: String, required: true},
+  default_price: {type: String, required: true},
+  created_at: {type: String, required: true},
+  updated_at: {type: String, required: true},
+  features: {type: Array, required: true}
+});
+
+let styleSchema = new mongoose.Schema({
+  styles: {type: Array}
+})
+
+let reviewSchema = new mongoose.Schema({
+  reviews: {type: Array}
+})
+
+let cartSchema = new mongoose.Schema({
+  cartData: {type: Array}
+})
