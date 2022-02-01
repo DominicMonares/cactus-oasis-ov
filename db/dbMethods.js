6bd71e08f2423def5626da90dfdb248a1234df7c
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-01-31 21:35:36 -0800

    save progress

--- db/dbMethods.js
+++ db/dbMethods.js
@@ -1,5 +1,17 @@
+const {Product, Style, Review, ReviewPhoto, Cart} = require('./index.js');
+
+let createProduct = (product) => {
+
+}
+
+let fetchProduct = (product_id) => {
+
+}
+
+
 // fetch product data
 // fetch product style data
 // fetch product review data
 // fetch cart data
 // add product to cart
+
