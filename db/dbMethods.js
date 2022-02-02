8c7d1f4d3e384ee068333c207f432e6e5ddb4184
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-01 16:37:05 -0800

    stub out db methods

--- db/dbMethods.js
+++ db/dbMethods.js
@@ -8,10 +8,48 @@ let fetchProduct = (product_id) => {
 
 }
 
+/* ========== */
+
+let createStlye = (style) => {
+
+}
+
+let fetchStyle = (style_id) => {
+
+}
+
+/* ========== */
+
+let createReview = (review) => {
+
+}
+
+let fetchReview = (review_id) => {
+
+}
+
+/* ========== */
+
+let createReviewPhoto = (review_photo) => {
+
+}
+
+let fetchReviewPhoto = (review_photo) => {
+
+}
+
+/* ========== */
+
+let addToCart = (product) => {
+
+}
+
+let clearCart = (product) => {
+
+}
 
 // fetch product data
 // fetch product style data
 // fetch product review data
 // fetch cart data
 // add product to cart
-
