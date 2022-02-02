89f20b6a997ea27138a5b8a92bef37bc15bf652b
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-01 17:27:34 -0800

    create test product creation

--- db/dbMethods.js
+++ db/dbMethods.js
@@ -1,7 +1,23 @@
 const {Product, Style, Review, ReviewPhoto, Cart} = require('./index.js');
 
-let createProduct = (product) => {
+let createProduct = async (product) => {
   // not used client side
+  let newProduct = new Product({
+    id: 1,
+    campus: 'hr-rpp',
+    name: '2001 Honda Civic',
+    slogan: 'Yoda\'s whip.',
+    description: 'Yoda has committed horrible crimes using this vehicle.',
+    category: 'Sweet Automobiles',
+    default_price: '1200.00',
+    updated_at: '2021-10-18T22:50:41.839Z',
+    created_at: '2021-10-18T22:50:41.839Z',
+    features: [
+      {feature: 'Interior', value: 'Rad as hell'}
+    ]
+  });
+
+  await newProduct.save();
 }
 
 let fetchProduct = (product_id) => {
