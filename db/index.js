25061baedfb41fd25704658b677931e105ea5435
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-01 21:38:38 -0800

    remove async connection

--- db/index.js
+++ db/index.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/SDC')
-}
+mongoose.connect('mongodb://localhost:27017/SDC');
 
 const productSchema = new mongoose.Schema({
   id: {type: Number, required: true, unique: true},
