49c1df86df139336df0041bc18eb23563b6a1e94
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-03 23:49:22 -0800

    add id properties

--- db/index.js
+++ db/index.js
@@ -12,12 +12,14 @@ const productSchema = new mongoose.Schema({
 });
 
 const featureSchema = new mongoose.Schema({
+  id: {type: Number, required: true, unique: true},
   product_id: {type: Number, required: true},
   feature: {type: String, required: true},
   value: {type: String, required: true}
 });
 
 const styleSchema = new mongoose.Schema({
+  id: {type: Number, required: true, unique: true},
   product_id: {type: String, required: true},
   style_id: {type: Number, required: true},
   name: {type: String, required: true},
@@ -27,18 +29,21 @@ const styleSchema = new mongoose.Schema({
 });
 
 const photoSchema = new mongoose.Schema({
+  id: {type: Number, required: true, unique: true},
   style_id: {type: String, required: true},
   thumbnail_url: {type: String, required: true},
   url: {type: String, required: true}
 });
 
 const skuSchema = new mongoose.Schema({
+  id: {type: Number, required: true, unique: true},
   style_id: {type: String, required: true},
   size: {type: String, required: true},
   quantity: {type: Number, required: true}
 });
 
 const reviewSchema = new mongoose.Schema({
+  id: {type: Number, required: true, unique: true},
   product_id: {type: String, required: true},
   review_id: {type: Number, required: true},
   rating: {type: Number, required: true},
