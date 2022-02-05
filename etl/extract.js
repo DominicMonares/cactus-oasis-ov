26b0122fd51c08f93d22743f3fbc993cdfbd1ac2
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-04 21:21:01 -0800

    review etl prep

--- etl/extract.js
+++ etl/extract.js
@@ -98,7 +98,7 @@ let extractSKUs = () => {
 }
 
 let extractReviews = () => {
-  const csvFilePath = `${__dirname}/origin/split/reviews/reviews1.csv`;
+  const csvFilePath = `${__dirname}/origin/split/reviews/reviews2.csv`;
   return csv()
     .fromFile(csvFilePath)
     .then(data => {
