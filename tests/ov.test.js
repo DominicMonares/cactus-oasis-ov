f228a6e2ab228a5aac36d9828c19e8847b3a500b
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-09 23:21:59 -0800

    implement test objects

--- tests/ov.test.js
+++ tests/ov.test.js
@@ -1,13 +1,13 @@
-const app = require('../server/index');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const _ = require('underscore');
 
+const createServer = require('../server/server.js');
 const {clientRoutes} = require('../server/clientRoutes.js');
 const {Product, Feature, Style, Photo, SKU, Review, ReviewPhoto, Cart} = require('../db/index.js');
 
 // SAMPLES MAY NOT BE NEEDED, REVISIT
-// const {productData, styleData, reviewData, cartData} = require('./samples.js');
+const {testProduct, testFeature} = require('./testObjects.js');
 
 describe('Overview API', () => {
 
@@ -15,7 +15,7 @@ describe('Overview API', () => {
     mongoose.connect('mongodb://localhost:27017/SDCTest', {
       useNewUrlParser: true,
       useUnifiedTopology: true
-    }, () => done());
+    }, () => done())
   });
 
   afterEach(done => {
@@ -24,11 +24,49 @@ describe('Overview API', () => {
     })
   });
 
-  describe('Products', () => {
-    // test('GET /products', async () => {
-    //   const products = await
-    // })
+  const app = createServer();
+
+  /* ========== PRODUCTS ========== */
+
+  describe.only('Products', () => {
+
+
+    test('GET /products', async () => {
+      const product = await Product.create(testProduct);
+      await supertest(app)
+        .get('/products')
+        .expect(200)
+        .then(response => {
+          expect(Array.isArray(response.body)).toBeTruthy();
+          expect(response.body.length).toEqual(1);
+          expect(response.body[0]['id']).toBe(product.id);
+          expect(response.body[0]['name']).toBe(product.name);
+          expect(response.body[0]['slogan']).toBe(product.slogan);
+          expect(response.body[0]['description']).toBe(product.description);
+          expect(response.body[0]['category']).toBe(product.category);
+          expect(response.body[0]['default_price']).toBe(product.default_price);
+        })
+    })
+
+    test('GET /products/:product_id', async () => {
+      const product = await Product.create(testProduct);
+      await supertest(app)
+        .get('/products/1')
+        .expect(200)
+        .then(response => {
+          expect(Array.isArray(response.body)).toBeTruthy();
+          expect(response.body.length).toEqual(1);
+          expect(response.body[0]['id']).toBe(product.id);
+          expect(response.body[0]['name']).toBe(product.name);
+          expect(response.body[0]['slogan']).toBe(product.slogan);
+          expect(response.body[0]['description']).toBe(product.description);
+          expect(response.body[0]['category']).toBe(product.category);
+          expect(response.body[0]['default_price']).toBe(product.default_price);
+        })
+    })
   })
+
+  /* ========== STYLES ========== */
 
   describe('Styles', () => {
     test('sample test', () => {
@@ -38,6 +76,8 @@ describe('Overview API', () => {
     })
   })
 
+  /* ========== REVIEWS ========== */
+
   describe('Reviews', () => {
     test('sample test', () => {
       let testResult = styleData;
@@ -45,6 +85,8 @@ describe('Overview API', () => {
       expect(typeof testResult).toBe('object');
     })
   })
+
+  /* ========== CART ========== */
 
   describe('Cart', () => {
     // POST WILL NEED TO DELETE ITEM IT JUST ADDED AFTER TEST
@@ -54,4 +96,5 @@ describe('Overview API', () => {
       expect(typeof testResult).toBe('object');
     })
   })
+
 })
---
+++ tests/testObjects.js
@@ -0,0 +1,20 @@
+const testProduct = {
+  id: 1,
+  name: 'Test product',
+  slogan: 'Test slogan',
+  description: 'Test description',
+  category: 'Test category',
+  default_price: 'Test price'
+};
+
+const testFeature = {
+  id: 1,
+  product_id: 1,
+  feature: 'Test feature',
+  value: 'Test value'
+}
+
+module.exports = {
+  'testProduct': testProduct,
+  'testFeature': testFeature
+}
