168a3518ca755647ed0085af7bdc1a176fb8d5cf
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-09 17:09:51 -0800

    uninstall frisby and joi, install supertest

--- tests/ov.test.js
+++ tests/ov.test.js
@@ -1,19 +1,51 @@
 const _ = require('underscore');
 const {productData, styleData, reviewData, cartData} = require('./samples.js');
+const {clientRoutes} = require('../server/clientRoutes.js');
 
 describe('Overview API', () => {
 
-  test('test test', () => {
-    let testResult = _.map([1, 2, 3], val => {
-      return val;
+  // test('test test', () => {
+  //   let testResult = _.map([1, 2, 3], val => {
+  //     return val;
+  //   })
+  //   expect(testResult).toStrictEqual(testResult);
+  // })
+
+  // test('sample test', () => {
+  //   let testResult = styleData;
+  //   console.log(testResult);
+  //   expect(typeof testResult).toBe('object');
+  // })
+
+  describe('Products', () => {
+    test('sample test', () => {
+      let testResult = styleData;
+      console.log(testResult);
+      expect(typeof testResult).toBe('object');
     })
-    expect(testResult).toStrictEqual(testResult);
   })
 
-  test('sample test', () => {
-    let testResult = styleData;
-    console.log(testResult);
-    expect(typeof testResult).toBe('object');
+  describe('Styles', () => {
+    test('sample test', () => {
+      let testResult = styleData;
+      console.log(testResult);
+      expect(typeof testResult).toBe('object');
+    })
   })
 
+  describe('Reviews', () => {
+    test('sample test', () => {
+      let testResult = styleData;
+      console.log(testResult);
+      expect(typeof testResult).toBe('object');
+    })
+  })
+
+  describe('Cart', () => {
+    test('sample test', () => {
+      let testResult = styleData;
+      console.log(testResult);
+      expect(typeof testResult).toBe('object');
+    })
+  })
 })
