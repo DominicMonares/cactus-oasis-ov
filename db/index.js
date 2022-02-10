3db58c2d71355699190a9cba982d9584a762d6d5
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-09 19:21:40 -0800

    add connection options

--- db/index.js
+++ db/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/SDC');
+mongoose.connect('mongodb://localhost:27017/SDC', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const productSchema = new mongoose.Schema({
   id: {type: Number, required: true, unique: true},
