6f7bc7b7a40ac3a1f2ebd84d4ed88f6e72a0ecb1
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-10 00:27:34 -0800

    implement test objects and style test

--- tests/ov.test.js
+++ tests/ov.test.js
@@ -7,7 +7,7 @@ const {clientRoutes} = require('../server/clientRoutes.js');
 const {Product, Feature, Style, Photo, SKU, Review, ReviewPhoto, Cart} = require('../db/index.js');
 
 // SAMPLES MAY NOT BE NEEDED, REVISIT
-const {testProduct, testFeature} = require('./testObjects.js');
+const {testProduct, testFeature, testStyle, testPhoto, testSKU, testReview, testReviewPhoto, testCart} = require('./testObjects.js');
 
 describe('Overview API', () => {
 
@@ -28,40 +28,41 @@ describe('Overview API', () => {
 
   /* ========== PRODUCTS ========== */
 
-  describe.only('Products', () => {
-
-
+  describe('Products', () => {
     test('GET /products', async () => {
       const product = await Product.create(testProduct);
       await supertest(app)
         .get('/products')
         .expect(200)
         .then(response => {
-          expect(Array.isArray(response.body)).toBeTruthy();
-          expect(response.body.length).toEqual(1);
-          expect(response.body[0]['id']).toBe(product.id);
-          expect(response.body[0]['name']).toBe(product.name);
-          expect(response.body[0]['slogan']).toBe(product.slogan);
-          expect(response.body[0]['description']).toBe(product.description);
-          expect(response.body[0]['category']).toBe(product.category);
-          expect(response.body[0]['default_price']).toBe(product.default_price);
+          let res = response.body;
+          expect(Array.isArray(res)).toBeTruthy();
+          expect(res.length).toEqual(1);
+          expect(res[0]['id']).toBe(product.id);
+          expect(res[0]['name']).toBe(product.name);
+          expect(res[0]['slogan']).toBe(product.slogan);
+          expect(res[0]['description']).toBe(product.description);
+          expect(res[0]['category']).toBe(product.category);
+          expect(res[0]['default_price']).toBe(product.default_price);
         })
     })
 
     test('GET /products/:product_id', async () => {
       const product = await Product.create(testProduct);
+      const feature = await Feature.create(testFeature);
       await supertest(app)
         .get('/products/1')
         .expect(200)
         .then(response => {
-          expect(Array.isArray(response.body)).toBeTruthy();
-          expect(response.body.length).toEqual(1);
-          expect(response.body[0]['id']).toBe(product.id);
-          expect(response.body[0]['name']).toBe(product.name);
-          expect(response.body[0]['slogan']).toBe(product.slogan);
-          expect(response.body[0]['description']).toBe(product.description);
-          expect(response.body[0]['category']).toBe(product.category);
-          expect(response.body[0]['default_price']).toBe(product.default_price);
+          let res = response.body;
+          expect(typeof res === 'object').toBeTruthy();
+          expect(Object.keys(res).length).toEqual(7);
+          expect(res.id).toBe(product.id);
+          expect(res.name).toBe(product.name);
+          expect(res.slogan).toBe(product.slogan);
+          expect(res.description).toBe(product.description);
+          expect(res.category).toBe(product.category);
+          expect(res.default_price).toBe(product.default_price);
         })
     })
   })
@@ -69,16 +70,48 @@ describe('Overview API', () => {
   /* ========== STYLES ========== */
 
   describe('Styles', () => {
-    test('sample test', () => {
-      let testResult = styleData;
-      console.log(testResult);
-      expect(typeof testResult).toBe('object');
+    test('GET /products/:product_id/styles', async () => {
+      const product = await Product.create(testProduct);
+      const feature = await Feature.create(testFeature);
+      const style = await Style.create(testStyle);
+      const photo = await Photo.create(testPhoto);
+      const sku = await SKU.create(testSKU);
+      await supertest(app)
+        .get('/products/1/styles')
+        .expect(200)
+        .then(response => {
+          let res = response.body;
+          let results = res.results;
+          let photos = results[0]['photos'];
+          let skus = results[0]['skus'];
+          expect(typeof res === 'object').toBeTruthy();
+          expect(Object.keys(res).length).toEqual(2);
+
+          expect(Array.isArray(results)).toBeTruthy();
+          expect(results.length).toEqual(1);
+          expect(results[0]['style_id']).toBe(style.style_id);
+          expect(results[0]['name']).toBe(style.name);
+          expect(results[0]['sale_price']).toBe(style.sale_price);
+          expect(results[0]['original_price']).toBe(style.original_price);
+          expect(results[0]['default?']).toBe(style['default?']);
+
+          expect(Array.isArray(photos)).toBeTruthy();
+          expect(photos.length).toEqual(1);
+          expect(photos[0]['thumbnail_url']).toBe(photo.thumbnail_url);
+          expect(photos[0]['url']).toBe(photo.url);
+
+          expect(typeof skus === 'object').toBeTruthy();
+          expect(Object.keys(skus).length).toEqual(1);
+          expect(Number(Object.keys(skus)[0])).toBe(sku['id'])
+          expect(skus['1']['quantity']).toBe(sku['quantity']);
+          expect(skus['1']['size']).toBe(sku['size']);
+        })
     })
   })
 
   /* ========== REVIEWS ========== */
 
-  describe('Reviews', () => {
+  xdescribe('Reviews', () => {
     test('sample test', () => {
       let testResult = styleData;
       console.log(testResult);
@@ -88,7 +121,7 @@ describe('Overview API', () => {
 
   /* ========== CART ========== */
 
-  describe('Cart', () => {
+  xdescribe('Cart', () => {
     // POST WILL NEED TO DELETE ITEM IT JUST ADDED AFTER TEST
     test('sample test', () => {
       let testResult = styleData;
--- tests/testObjects.js
+++ tests/testObjects.js
@@ -12,9 +12,66 @@ const testFeature = {
   product_id: 1,
   feature: 'Test feature',
   value: 'Test value'
+};
+
+const testStyle = {
+  style_id: 1,
+  product_id: 1,
+  name: 'Test name',
+  sale_price: 'Test sale price',
+  original_price: 'Test original price',
+  'default?': true
 }
+
+const testPhoto = {
+  id: 1,
+  style_id: 1,
+  thumbnail_url: 'Test thumbnail',
+  url: 'Test url'
+};
+
+const testSKU = {
+  id: 1,
+  style_id: 1,
+  size: 'Test size',
+  quantity: 1
+};
+
+const testReview = {
+  review_id: 1,
+  product_id: 1,
+  rating: 5,
+  summary: 'Test summary',
+  recommend: true,
+  response: 'Test response',
+  body: 'Test body',
+  date: 'Test date',
+  reviewer_name: 'Test reviewer',
+  email: 'Test email',
+  helpfulness: 4,
+  reported: false
+};
+
+const testReviewPhoto = {
+  id: 1,
+  review_id: 1,
+  url: 'Test url'
+};
+
+const testCart = {
+  id: 1,
+  user_session: 3232,
+  product_id: 1,
+  active: 1
+};
 
 module.exports = {
   'testProduct': testProduct,
-  'testFeature': testFeature
+  'testFeature': testFeature,
+  'testStyle': testStyle,
+  'testPhoto': testPhoto,
+  'testSKU': testSKU,
+  'testReview': testReview,
+  'testReviewPhoto': testReviewPhoto,
+  'testCart': testCart
 }
