a9ddf84068b033a06190e17b35815b5dc2dcbe70
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-09 22:20:11 -0800

    modularize server initialization

--- db/index.js
+++ db/index.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/SDC', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
 const productSchema = new mongoose.Schema({
   id: {type: Number, required: true, unique: true},
   name: {type: String, required: true},
