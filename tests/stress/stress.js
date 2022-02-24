07811ddedb9d216667f8bb317602159431a15de7
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-24 00:47:14 -0800

    implement ramp up

--- tests/stress/stress.js
+++ tests/stress/stress.js
@@ -2,14 +2,26 @@ import http from 'k6/http';
 import {sleep} from 'k6';
 
 export const options = {
-  stages: [
-    {target: 1000, duration: '30s'}
-  ]
+  discardResponseBodies: true,
+  scenarios: {
+    contacts: {
+      executor: 'ramping-vus',
+      startVUs: 0,
+      stages: [
+        {target: 1, duration: '5s'},
+        {target: 10, duration: '5s'},
+        {target: 100, duration: '5s'},
+        {target: 1000, duration: '5s'},
+        {target: 0, duration: '25s'}
+      ],
+      gracefulRampDown: '0s'
+    }
+  }
 };
 
 export default function () {
-  // http.get('http://localhost:8080/cart');
+  http.get('http://localhost:8080/cart');
 
-  http.post('http://localhost:8080/cart/?sku_id=10000000');
+  // http.post('http://localhost:8080/cart/?sku_id=10000000');
   sleep(1);
 }
