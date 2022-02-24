6fb426b5e9e4ef7577929b331116e17116d9ad51
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-24 00:13:28 -0800

    add newrelic

--- tests/stress/stress.js
+++ tests/stress/stress.js
@@ -3,11 +3,13 @@ import {sleep} from 'k6';
 
 export const options = {
   stages: [
-    {target: 100, duration: '30s'}
+    {target: 1000, duration: '30s'}
   ]
 };
 
 export default function () {
-  http.get('http://localhost:8080/products/1000000');
+  // http.get('http://localhost:8080/cart');
+
+  http.post('http://localhost:8080/cart/?sku_id=10000000');
   sleep(1);
 }
