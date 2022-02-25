59554b6c9f119577222905e5e949c036b12a859a
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-24 20:56:48 -0800

    save progress

--- tests/stress/stress.js
+++ tests/stress/stress.js
@@ -2,26 +2,16 @@ import http from 'k6/http';
 import {sleep} from 'k6';
 
 export const options = {
-  discardResponseBodies: true,
-  scenarios: {
-    contacts: {
-      executor: 'ramping-vus',
-      startVUs: 0,
-      stages: [
-        {target: 1, duration: '5s'},
-        {target: 10, duration: '5s'},
-        {target: 100, duration: '5s'},
-        {target: 1000, duration: '5s'},
-        {target: 0, duration: '20s'}
-      ],
-      gracefulRampDown: '0s'
-    }
-  }
+  stages: [
+    { duration: '15s', target: 1000 },
+    { duration: '30s', target: 1000 },
+    { duration: '15s', target: 0 }
+  ],
 };
 
 export default function () {
-  http.get('http://localhost:8080/cart');
+  // http.get('http://localhost:8080/cart');
 
-  // http.post('http://localhost:8080/cart/?sku_id=10000000');
+  http.post('http://localhost:8080/cart/?sku_id=10000000');
   sleep(1);
 }
