5e8e239630a878b4dec63054a8a3b35443c24ebb
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-02-26 19:13:11 -0800

    modify tests

--- tests/stress/stress.js
+++ tests/stress/stress.js
@@ -3,15 +3,15 @@ import {sleep} from 'k6';
 
 export const options = {
   stages: [
-    { duration: '15s', target: 1000 },
-    { duration: '30s', target: 1000 },
-    { duration: '15s', target: 0 }
+    { duration: '10s', target: 2000 },
+    { duration: '20s', target: 2000 },
+    { duration: '10s', target: 0 }
   ],
 };
 
 export default function () {
-  // http.get('http://localhost:8080/cart');
+  http.get('http://localhost:8080/products/999456');
 
-  http.post('http://localhost:8080/cart/?sku_id=10000000');
+  // http.post('http://localhost:8080/cart/?sku_id=10000000');
   sleep(1);
 }
