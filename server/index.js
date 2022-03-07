f5b2ee2cf0c6b80bf20456a3f8e15a87b28a765f
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-03-06 17:26:18 -0800

    loader.io setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const createServer = require('./server.js');
 
+// .connect('mongodb://localhost:27017/SDC', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+
 mongoose
-  .connect('mongodb://localhost:27017/SDC', {
+  .connect(`mongodb://dominic:${process.env.DB_PW}@54.166.134.118/SDC`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -13,3 +19,7 @@ mongoose
       console.log('Listening on port 8080');
     })
 });
+
+additionalRouter.get('/loaderio-167d73d03ab32c3fcf35f58f2aba4fe5/', (req, res) => {
+  res.send('loaderio-167d73d03ab32c3fcf35f58f2aba4fe5');
+})
