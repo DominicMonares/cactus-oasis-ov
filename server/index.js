950a4ef9dd01e60a552d99f45670438b0b0cc77a
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-03-06 17:41:35 -0800

    change connection to local for testing

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const createServer = require('./server.js');
 
-// .connect('mongodb://localhost:27017/SDC', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
+// .connect(`mongodb://dominic:${process.env.DB_PW}@54.166.134.118/SDC`, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true
+  // })
 
-mongoose
-  .connect(`mongodb://dominic:${process.env.DB_PW}@54.166.134.118/SDC`, {
+  mongoose
+  .connect('mongodb://localhost:27017/SDC', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -20,6 +20,6 @@ mongoose
     })
 });
 
-additionalRouter.get('/loaderio-167d73d03ab32c3fcf35f58f2aba4fe5/', (req, res) => {
-  res.send('loaderio-167d73d03ab32c3fcf35f58f2aba4fe5');
-})
+// additionalRouter.get('/loaderio-167d73d03ab32c3fcf35f58f2aba4fe5/', (req, res) => {
+//   res.send('loaderio-167d73d03ab32c3fcf35f58f2aba4fe5');
+// })
